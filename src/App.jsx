2fd646fa45ad0c54366e0e0707698c990ed22fd4
TinNguyen <ductin16111@gmail.com>
2023-11-18 19:15:11 +0200

    add function handleAddButton() and update UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,37 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isFormOpen, setIsFormOpen] = useState(false)
     const [title, setTitle] = useState("")
     const [deadline, setDeadline] = useState("")
     const [status, setStatus] = useState("status")
     const [todoList, setTodoList] = useState([])
 
+    function handleAddButton() {
+        setIsFormOpen(false)
+
+        const newTodo = {title: title, deadline: deadline, status: status}
+        setTodoList([...todoList, newTodo])
+    }
+
     return (
         <>
-            <button onClick={() => {setIsOpen(true)}}>Add a new todo</button>
-            {isOpen && <form>
-                <input type="text" name="title" value={title} placeholder='Title' onChange={(e) => setTitle(e.target.value)}/>
-                <input type="text" name="deadline" value={deadline} placeholder='Deadline' onChange={(e) => setDeadline(e.target.value)} />
-                <select value={status} onChange={(e) => setStatus(e.target.value)}>
+            <button onClick={() => {setIsFormOpen(true)}}>Add a new todo</button>
+            {isFormOpen && <form id="todo-form">
+                <input type="text" name="title" placeholder='Title' onChange={(e) => setTitle(e.target.value)}/>
+                <input type="text" name="deadline" placeholder='Deadline' onChange={(e) => setDeadline(e.target.value)} />
+                <select onChange={(e) => setStatus(e.target.value)}>
                     <option value="status">Status</option>
-                    <option value="vegetable">Vegetable</option>
-                    <option value="meat">Meat</option>
+                    <option value="In progress">In progress</option>
+                    <option value="Not started">Not started</option>
+                    <option value="Done">Done</option>
                 </select>
-                <input type="submit" value="Add" onClick={() => {setIsOpen(false)}} />
+                <input type="button" value="Add" onClick={() => handleAddButton()} />
+                <input type="button" value="Cancel" onClick={() => setIsFormOpen(false)} />
             </form>}
-            <p>{title} {deadline} {status}</p>
+            {(todoList.length > 0) ? <div>
+                {todoList.map((todo) => <p>{todo.title} {todo.deadline} {todo.status}</p>)}
+            </div> : <p></p>}
         </>
     )
 }
